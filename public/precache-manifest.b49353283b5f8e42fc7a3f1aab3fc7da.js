d899f31678ba585e273875d1ad4eb7b0362b884a
Shane Streator <shanestreator@gmail.com>
2019-03-19 06:57:58 -0500

    ready to deploy

---
+++ public/precache-manifest.b49353283b5f8e42fc7a3f1aab3fc7da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08565ec3a3ad203908f7",
+    "url": "/static/js/main.e5baef0e.chunk.js"
+  },
+  {
+    "revision": "fbd678611f51372e2019",
+    "url": "/static/js/2.c1f925ae.chunk.js"
+  },
+  {
+    "revision": "08565ec3a3ad203908f7",
+    "url": "/static/css/main.017aa0c3.chunk.css"
+  },
+  {
+    "revision": "aebe5da995595449649dadb6bf6afeba",
+    "url": "/index.html"
+  }
+];

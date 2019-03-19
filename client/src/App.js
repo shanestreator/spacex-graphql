ca5817f0ddb8cd4c914f126a6a6f8327e7e9be49
Shane Streator <shanestreator@gmail.com>
2019-03-19 04:12:12 -0500

    ready for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Launches from './Components/Launches'
 import Launch from './Components/Launch'
-import './App.css';
+import './App.css'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 })
 
 class App extends Component {
@@ -16,14 +16,19 @@ class App extends Component {
       <ApolloProvider client={client}>
         <Router>
           <div className="container">
-            <img className='p-3' src="/images/logo.webp" alt="spacex logo" style={{width:300, display: 'block', margin: 'auto'}} />
-            <Route exact path='/' component={Launches} />
-            <Route exact path='/launch/:flight_number' component={Launch} />
+            <img
+              className="p-3"
+              src="/images/logo.webp"
+              alt="spacex logo"
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
           </div>
         </Router>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
